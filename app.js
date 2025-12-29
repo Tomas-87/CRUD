10d40d78b8b14bf624367977feba519ceee545b6
Tomas <tomascarrerasalbesa@gmail.com>
2025-12-29 21:46:47 +0100

    mas optimizado con todos los usuarios y un usuario por nombre juntos

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3100;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json; Charset=utf-8');
+  next();
+})
 
 let usuarios = [
     { id: 1, nombre: 'Ryu', edad: 32, lugarProcedencia: 'Japón' },
@@ -12,26 +16,26 @@ let usuarios = [
     { id: 4, nombre: 'Dhalsim', edad: 45, lugarProcedencia: 'India' },
     { id: 5, nombre: 'Blanka', edad: 32, lugarProcedencia: 'Brasil' },
 ];
- //todos los usuarios
-app.get('/', (req, res) => {
-    res.json(usuarios);
+
+//todos los usuarios y usuarios por nombre
+app.get('/usuarios', (req, res) => {
+  const { nombre } = req.query;
+
+  if (!nombre) {
+    return res.json(usuarios);
+  };
+  const usuarioFilter = usuarios.filter(n => n.nombre.toLowerCase() === nombre.toLowerCase());
+
+  if (!usuarioFilter.length) {
+    return res.status(404).json('No hay usuarios con ese nombre');
+  };
+  res.json(usuarioFilter);
 });
 
-//usuario por nombre
-app.get('/usuarios/:nombre', (req, res) => {
-  const usuario = usuarios.find(usu => usu.nombre.toLowerCase() === req.params.nombre.toLowerCase());
-
-  if (!usuario) {
-    return res.status(404).json({ ERROR: 'Usuario no encontrado' })
-  }
-  res.json(usuario);
-});
-
-
-//crear un nuevo usuario
+//añadir usuario
 app.post('/usuarios', (req, res) => {
   const { nombre, edad, lugarProcedencia } = req.body;
-
+  
   const nuevoUsuario = {
     id: usuarios.length + 1,
     nombre,
@@ -39,37 +43,42 @@ app.post('/usuarios', (req, res) => {
     lugarProcedencia,
   };
   usuarios.push(nuevoUsuario);
-  res.status(201).json(nuevoUsuario);
+  res.status(201).json({
+    Mensaje: 'Usuario creado correctamente',
+    Usuario: nuevoUsuario
+  });
 });
 
-//actualizar un usuario por nombre
+//cambiar algo de un usuario
 app.put('/usuarios/:nombre', (req, res) => {
-  const index = usuarios.findIndex(usu => usu.nombre.toLowerCase() === req.params.nombre.toLowerCase());
-
-  if (index === -1) {
-    return res.status(404).json({ ERROR: 'No pudimos encontrar al Usuario'});
-    }
-
-    usuarios[index] = {
-      ...usuarios[index],
-      ...req.body,
-    };
-    res.json(usuarios[index]);
-});
-
-//eliminar usuarios por nombre
-app.delete('/usuarios/:nombre', (req, res) => {
-  const existe = usuarios.some(usu => usu.nombre.toLowerCase() === req.params.nombre.toLowerCase());
-
-  if (!existe) {
-    return res.status(404).json({ ERROR: 'El Usuario no Existe' });
+  const index = usuarios.findIndex(u => u.nombre.toLowerCase() === req.params.nombre.toLowerCase());
+  
+  if (index ===- 1) {
+    return res.status(404).json({ ERROR: 'Usuario no encontrado'});
+  }
+  const { edad, lugarProcedencia } = req.body;
+  usuarios[index] = {
+    ...usuarios[index],
+    ...req.body
   };
-
-  usuarios = usuarios.filter(usu => usu.nombre.toLowerCase() !== req.params.nombre.toLowerCase());
-
-  res.json({ MENSAJE: 'Se ha Eliminado al Usuario correctamente' });
+  res.json(usuarios[index]);
 });
+
+//eliminar usuario
+app.delete('/usuarios/:id', (req, res) => {
+  const { id } = req.body;
+  const idNum = Number(id);
+
+  const existe = usuarios.some(s => s.id === idNum);
+  if(!existe) {
+    return res.status(401).json('El id no existe');
+  };
+  usuarios = usuarios.filter(d => d.id !== idNum);
+  
+  res.json('El usuario fue eliminado correctamente');
+});
+
 
 app.listen(PORT, () => {
-    console.log(`El servidor escucha en el puerto http://localhost:${PORT}`)
+  console.log(`El servidor esta escuchando en el puerto ${PORT}`);
 });
